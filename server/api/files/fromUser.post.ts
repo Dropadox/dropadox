9250419c8f7db1fcd83b9e2e19679a2071452507
Julian Seitz <julian3seitz@gmail.com>
2025-11-29 14:53:11 -0500

    Add API endpoint to fetch user uploads
    Introduces a new POST endpoint at /api/files/fromUser to retrieve uploads for a user based on a JWT token. Adds utility functions for extracting user information from the token and querying the database. Also includes a Bruno API test for the new endpoint.

---
+++ server/api/files/fromUser.post.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import { uploads } from '~~/server/database/schema';
+import { eq } from 'drizzle-orm';
+import { getUserFromPayload, getUserPayload } from '~~/server/utils/getUser';
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+
+    const { token } = body;
+
+    if (!token) {
+        throw createError({
+            statusText: "No token provided",
+            status: 400,
+        })
+    }
+
+    const userPayload = getUserPayload(token);
+    const user = await getUserFromPayload(userPayload);
+
+    if (!user) {
+        throw createError({
+            statusText: "No user found.",
+            status: 404
+        });
+    }
+
+    const userUploads = await useDrizzle().select().from(uploads)
+        .where(eq(uploads.userId, user.id.toString()))
+        .all()
+
+    return { userUploads }
+});
---
+++ server/utils/getUser.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import { UserPayload } from '~~/shared/types/UserPayload';
+import { users } from '../database/schema';
+import { eq } from 'drizzle-orm';
+import { User } from './useDrizzle';
+
+export function getUserPayload(token: string): UserPayload {
+    const userPayload = jwt.verify(token, process.env.JSON_SECRET_KEY!);
+
+    return userPayload as UserPayload;
+}
+
+export async function getUserFromPayload(userPayload: UserPayload) {
+    const user = await useDrizzle().select().from(users)
+        .where(eq(users.id, Number(userPayload.id))).get();
+
+    return user;
+}
