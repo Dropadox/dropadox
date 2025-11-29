948a2300246e80635b38bc013aa8b7611ce03d6d
Mattlavelle966 <mattlavelle966@gmail.com>
2025-11-29 12:11:47 -0500

    complete ts upload

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -4,6 +4,21 @@ import { defineEventHandler, readMultipartFormData } from "h3";
 const upload = multer({ dest:"uploads/"})
 
 export default defineEventHandler( async (event) => {
-    const formdata = readMultipartFormData(event); 
+    const formdata = await readMultipartFormData(event); 
     
+    if (!formdata){
+        return {error:"failded to grab form data"};
+    }
+    const file = formdata.find(item => item.filename);
+
+    if (!file){
+        return {error:"failed to get file"}
+    }
+
+    return {
+        name: file.filename,
+        type: file.type,
+        size: file.data.length
+    };
+
 })
